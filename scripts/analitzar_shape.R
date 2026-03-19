f05360718ddb59dcbe71bece30c7e06c90e54c9a
Amorós Mèlich <oamoros3@edu.gencat.cat>
2026-03-19 12:12:53 +0100

    commit

--- scripts/analitzar_shape.R
+++ scripts/analitzar_shape.R
@@ -12,3 +12,33 @@ library(readr)
 
 riu <- st_read("data/raw/RIUS/PROVA/prova.shp")
 urbanisme <- st_read("data/raw/TERRITORI/URBANISME_Cat_prova.shp")
+
+
+# OPERACIONS BÀSIQUES
+
+#   -) SELCCIÓ COLUMNES
+#   -) FILTRAR
+#   -) AFEGIR COLUMNES
+
+# EXERCICI 1:
+
+# Creeo un DATAFRAME de MUNICIPIS 
+# FILTRO = CODIMUNI, NOM i Geometria
+
+
+municipis <- urbanisme %>% select(CODIMUNI, NOM)
+
+bcn <- municipis %>%
+  filter(NOM == "Barcelona")
+
+plot(bcn)
+
+# EXERCICI 2:
+
+# Creeo un DATAFRAME de MUNICIPIS 
+# FILTRO = CODIMUNI, NOM i Geometria
+
+
+round(st_distance(municipis[1,], municipis[2,])/1000,1)
+
+
