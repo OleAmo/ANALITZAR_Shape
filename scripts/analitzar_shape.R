6d2c2682aee70d0f4960d5f7d9643fc15da8aad3
Amorós Mèlich <oamoros3@edu.gencat.cat>
2026-03-19 12:34:39 +0100

    commit

--- scripts/analitzar_shape.R
+++ scripts/analitzar_shape.R
@@ -35,10 +35,44 @@ plot(bcn)
 
 # EXERCICI 2:
 
-# Creeo un DATAFRAME de MUNICIPIS 
-# FILTRO = CODIMUNI, NOM i Geometria
+# Creeo dues columens NOVES al DATAFRAME de MUNICIPIS 
+# Serà la distància a BCN en metres i Km
+# Dist_BCN_m i Dist_BCN_Km
+
+bcn <- municipis %>%
+  filter(NOM == "Barcelona")
 
 
-round(st_distance(municipis[1,], municipis[2,])/1000,1)
+# Creo Vectors DSITÀNCIA M
+# Creo Vector DISTÀNCIA KM
+
+dist_m <- c()
+dist_km <- c()
+for (i in 1:length(municipis$NOM)){
+  d_km <- round(st_distance(bcn, municipis[i,])/1000,2)
+  d_m <- round(st_distance(bcn, municipis[i,]),1)
+  
+  dist_m <- c(dist_m,d_m)
+  dist_km <- c(dist_km, d_km)
+}
+
+dist_m
+dist_km 
+
+
+# Un cop creat dels de de ADJUNTAR D'ALGUNA FORMA a MUNICIPIS
+# Com a COLUMNA M i COLUMNA KM
+
+
+
+municipis %>%         # Aqui creo columna DIST_BCN_M
+  mutate(             # Aquesta columna és la DIST de BCN al MUNICIPI 22
+    Dist_BCN_m =
+      round(st_distance(bcn, municipis[22,])/1000)
+      
+      )  
+
+# No se com AUTOMATIZAR el omplir columenes fent lso de ST_DISTANCE
+
 
 
